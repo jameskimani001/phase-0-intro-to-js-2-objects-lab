3f29ea65c316f3b20fe0d429ab34db74b4de4a2c
jameskimani001 <jameskim092t@gmail.com>
2024-10-03 11:36:05 +0300

    objects completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+
+const employee = {
+    name: "James",
+    streetAddress: "000023 Narobi"
+  };
+  
+  
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: _, ...newEmployee } = employee; 
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
